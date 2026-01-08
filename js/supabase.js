dd274f7de74c46c19f3bf8d97fc92788175c566f
Felipe Lopera <165114103+FelipeLopera11@users.noreply.github.com>
2026-01-08 18:57:03 -0500

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://sqptzzuzdxpygxnmhivy.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxcHR6enV6ZHhweWd4bm1oaXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1NTIxNDAsImV4cCI6MjA4MzEyODE0MH0.WvxJrIyKcPePwHBnOzHTOV8xpvlEg0PEiM8mw2DpyrE";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-
